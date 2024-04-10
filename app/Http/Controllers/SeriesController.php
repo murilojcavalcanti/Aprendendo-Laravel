bef5ec39583ad51b24aeca118898bc230b47e3e6
murilojcavalcanti <josemurilo7@gmail.com>
2024-04-10 18:53:32 -0300

    Aula sintaxe alternativa p1

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -15,48 +15,51 @@ class SeriesController extends Controller
     public function index(Request $request)
     {
         $series = Series::query()->orderBy('nome')->get();
-        $mensagemSucesso= session('mensagem.sucesso');
-        
-        return view('series.index',compact('series'))->with('mensagemSucesso',$mensagemSucesso);
+        $mensagemSucesso = session('mensagem.sucesso');
+
+        return view('series.index', compact('series'))->with('mensagemSucesso', $mensagemSucesso);
     }
-    public function create(){
+    public function create()
+    {
         return view('series.create');
     }
-    public function store(SeriesFromRequest $request){
+    public function store(SeriesFromRequest $request)
+    {
 
-        $serie=null;
+
         //usado para caso ocorra um erro no salvamento, não salve dados inconcistentes.
-        DB::transaction(function() use ($request,&$serie){        
-            $serie= Series::create($request->all());
-            $seasons=[];
-            for($i=1;$i<=$request->seasonsQty;$i++){
-                $seasons[]= [
-                    'series_id'=>$serie->id,
-                   'number'=>$i,
+        $serie = DB::transaction(function () use ($request, &$serie) {
+            $serie = Series::create($request->all());
+            $seasons = [];
+            for ($i = 1; $i <= $request->seasonsQty; $i++) {
+                $seasons[] = [
+                    'series_id' => $serie->id,
+                    'number' => $i,
                 ];
-            } 
+            }
             Season::insert($seasons);
-            $episodes=[];
-            foreach($serie ->seasons as $season){
-                for($j=1;$j<=$request->episodesPerSeason;$j++){
-                    $episodes[]=[
-                        'season_id'=>$season->id,
-                        'number'=>$j,
-                    ]; 
+            $episodes = [];
+            foreach ($serie->seasons as $season) {
+                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j,
+                    ];
                 }
             }
             Episode::insert($episodes);
+            return $serie;
         });
-      
-        return to_route('series.index')->with('mensagem.sucesso',"Série '{$serie-> nome}' adicionada com sucesso");
-            
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function edit(Series $series){
-        return view('series.edit')->with('serie',$series);
+    public function edit(Series $series)
+    {
+        return view('series.edit')->with('serie', $series);
     }
 
-    public function update (Series $series,SeriesFromRequest $request)
+    public function update(Series $series, SeriesFromRequest $request)
     {
         //dessa forma, quando tivermos mais atributos teremos que mudar repetir alinha 66 para todos
         //$series->nome=$request->nome;
@@ -65,15 +68,15 @@ class SeriesController extends Controller
         $series->fill($request->all());
         $series->save();
 
-        return to_route('series.index')->with('mensagem.sucesso',"serie: {$series->nome} atualizada com sucesso!");    
+        return to_route('series.index')->with('mensagem.sucesso', "serie: {$series->nome} atualizada com sucesso!");
     }
 
 
 
-    public function destroy(Series $series,Request $request){
+    public function destroy(Series $series, Request $request)
+    {
         $series->delete();
         //usando o metodo with a flash message já é usada
-        return to_route('series.index')->with('mensagem.sucesso',"Série '{$series->nome}' removida com sucesso");
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
     }
-    
 }
